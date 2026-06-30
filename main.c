94103bbdb4dbd9931b5d9de0b49058a6809e9716
emilybjb <emilybjb@gmail.com>
2026-06-30 15:22:03 -0300

    Implementa benchmark comparativo entre LRU e Árvore Splay

--- main.c
+++ main.c
@@ -3,12 +3,44 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define NUM_ACESSOS 10000
 #define CACHE_CAPACITY 8
+#define NUM_PAGINAS 64
 
-void testar_lru() {
+typedef enum {
+    PADRAO_UNIFORME,
+    PADRAO_ZIPFIANO
+} PadraoAcesso;
+
+uint64_t gerar_uniforme() {
+    return rand() % NUM_PAGINAS;
+}
+
+uint64_t gerar_zipfiano() {
+    double r = (double) rand() / RAND_MAX;
+
+    if (r < 0.50) {
+        return rand() % 4;          // páginas muito quentes
+    } else if (r < 0.80) {
+        return 4 + (rand() % 12);   // páginas médias
+    } else {
+        return 16 + (rand() % 48);  // páginas frias
+    }
+}
+
+uint64_t gerar_pagina(PadraoAcesso padrao) {
+    if (padrao == PADRAO_UNIFORME) {
+        return gerar_uniforme();
+    }
+
+    return gerar_zipfiano();
+}
+
+void testar_lru(PadraoAcesso padrao) {
     Cache* cache = cache_open("data.bin", CACHE_CAPACITY);
 
     if (cache == NULL) {
@@ -21,7 +53,7 @@ void testar_lru() {
     clock_t inicio = clock();
 
     for (int i = 0; i < NUM_ACESSOS; i++) {
-        uint64_t page_id = i % 16;
+        uint64_t page_id = gerar_pagina(padrao);
         cache_read(cache, page_id, buffer);
     }
 
@@ -36,7 +68,7 @@ void testar_lru() {
     cache_close(cache);
 }
 
-void testar_splay() {
+void testar_splay(PadraoAcesso padrao) {
     SplayCache* cache = splay_cache_open("data.bin", CACHE_CAPACITY);
 
     if (cache == NULL) {
@@ -49,7 +81,7 @@ void testar_splay() {
     clock_t inicio = clock();
 
     for (int i = 0; i < NUM_ACESSOS; i++) {
-        uint64_t page_id = i % 16;
+        uint64_t page_id = gerar_pagina(padrao);
         splay_cache_read(cache, page_id, buffer);
     }
 
@@ -65,14 +97,24 @@ void testar_splay() {
     splay_cache_close(cache);
 }
 
-int main() {
-    printf("Benchmark: LRU x Arvore Splay\n");
+void executar_benchmark(const char* nome, PadraoAcesso padrao) {
+    printf("\n\n=====================================\n");
+    printf("BENCHMARK: %s\n", nome);
     printf("Acessos: %d\n", NUM_ACESSOS);
     printf("Capacidade do cache: %d paginas\n", CACHE_CAPACITY);
-    printf("Padrao de acesso: conjunto maior que o cache, page_id = i %% 16\n");
+    printf("Total de paginas possiveis: %d\n", NUM_PAGINAS);
+    printf("=====================================\n");
 
-    testar_lru();
-    testar_splay();
+    srand(42);
+    testar_lru(padrao);
+
+    srand(42);
+    testar_splay(padrao);
+}
+
+int main() {
+    executar_benchmark("Carga uniforme", PADRAO_UNIFORME);
+    executar_benchmark("Carga zipfiana", PADRAO_ZIPFIANO);
 
     return 0;
 }
